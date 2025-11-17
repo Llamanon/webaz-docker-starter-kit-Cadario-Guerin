168d0ef3744c5009170e0b9c7f791cb49a02f2ca
Llamanon <manon.guerin@ensg.eu>
2025-11-17 19:37:09 +0100

    ajout_objets

--- apache-php/src/assets/map.js
+++ apache-php/src/assets/map.js
@@ -6,23 +6,37 @@ let osmLayer = new ol.layer.Tile({
     }),
 });
 
-let wmsLayer = new ol.layer.Tile({
-    source: new ol.source.TileWMS({
-        url: 'https://api.ensg.eu/geoserver/wms',
-        params: {
-            LAYERS: 'ensg:hydro',
-        },
-    })
+let test = new ol.Feature({
+  geometry: new ol.geom.Point(ol.proj.fromLonLat([5.78, 43.98]))
 });
+
+test.setStyle(
+  new ol.style.Style({
+    image: new ol.style.Icon({
+      src: 'assets/tacheo.jpg',
+      scale: 1
+    })
+  })
+);
+
+let tacheo = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    features: [test]
+  })
+});
+
 
 
 let map = new ol.Map({
     target: 'map',
     layers: [
-        osmLayer
+        osmLayer,
+        //tacheo
     ],
     view: new ol.View({
         center: ol.proj.fromLonLat([5.78, 43.96]),
         zoom: 15,
     }),
 });
+
+map.addLayer(tacheo);
