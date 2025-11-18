6158c581262b49630df69c7383569f400c0226e8
Llamanon <manon.guerin@ensg.eu>
2025-11-18 19:08:37 +0100

    essai_sql

--- apache-php/src/index.php
+++ apache-php/src/index.php
@@ -25,6 +25,26 @@ Flight::route('/test-db', function () {
     $sql = "SELECT * FROM points";
     $query = pg_query($link, $sql);
     $results = pg_fetch_all($query);
+
+    Flight::json($results);
+});
+
+Flight::route('/test-db2', function () {
+    $host = 'db';
+    $port = 5432;
+    $dbname = 'mydb';
+    $user = 'postgres';
+    $pass = 'postgres';
+
+    // Connexion BDD
+    $link = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
+
+    $sql2 = "INSERT INTO points (name, geom) VALUES
+        ('Forcalquier', ST_SetSRID(ST_MakePoint(5.78, 43.96), 4326));
+        SELECT * FROM points";
+    $query = pg_query($link, $sql2);
+    $results = pg_fetch_all($query);
+
     Flight::json($results);
 });
 
